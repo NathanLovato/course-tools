538aa4e57d8be535871b50dde7081ac47a5b5643
Nathan <nathan@gdquest.com>
2017-11-24 20:48:51 +0100

    Add regex-based identifier replacement

--- invoices/invoices.py
+++ invoices/invoices.py
@@ -1,36 +1,74 @@
 import json
 import re
+import logging
 
 # company_data = json.loads("company_data.json")
-
-data = {
+options = {
+    'exclude_VAT': True,
+    'currency_symbol': '€'
 }
 
+
 company = {
-    'name': 'Lovato Nathan',
-    'email': 'nathan@gdquest.com',
-    'address': '81 Place de la Mairie, 74550 Perrignier',
-    'VAT': 'FR09801047630',
+    'name': 'Test',
+    'email': 'myemail@test.com',
+    'address': 'My wonderful little address',
+    'VAT': '98869698'
 }
 
 client = {
-    'name': 'Lovato Nathan',
-    'email': 'test\@test.org',
-    'address': '81 Place de la Mairie, 74550 Perrignier',
-    'VAT': 'FR09801047630',
+    'name': 'Test',
+    'email': 'myemail@test.com',
+    'address': 'My wonderful little address',
+    'VAT': '98869698'
 }
 
-products: [
-    {
-        'id': 44,
-        'name': 'Cours Krita Pro',
-        'unit_price': 60,
-        'amount': 1
-    }
-]
+invoice_index = 1
 
+
+data = {
+    'company': company,
+    'client': client,
+    'date': '11-24-2017',
+    'products': [
+        {
+            'name': 'A wonderful pretty little thing',
+            'unit_price': 100,
+        }
+    ]
+}
+
+
+
+invoice_template = []
+re_matches = []
 with open('invoice.html', 'r') as html_doc:
+    line_id = 0
     for line in html_doc:
+        invoice_template.append(line)
+
         match = re.match(r'.+{{ (.*) }}', line)
         if match:
-            print(match.group(1))
+            re_matches.append((line_id, match.group(1)))
+        line_id += 1
+
+
+# TODO: store special matches like product_rows, payment_date, invoice_date
+# TODO: Discard invoice_number
+
+invoice_template_copy = invoice_template
+for index, identifier in re_matches:
+    string_template = '{{ ' + identifier + ' }}'
+    category, key = identifier.split('_', maxsplit=1)
+
+    try:
+        replace_value = data[category][key]
+    except:
+        replace_value = identifier
+        logging.warning('Could not find matching value for {!s}'.format(identifier))
+    invoice_template_copy[index] = invoice_template_copy[index].replace(string_template, replace_value, 1)
+
+
+# Create files
+with open('test_output.html', 'w') as html_doc:
+    html_doc.writelines(invoice_template_copy)
